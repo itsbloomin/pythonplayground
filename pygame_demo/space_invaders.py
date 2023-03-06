f9363da091b5170b3fdb56b28ff1182de13d9410
bloomin <steveb1@umbc.edu>
2023-03-06 16:11:42 -0500

    update to space_invaders.py: added Ship class, drew ship

--- pygame_demo/space_invaders.py
+++ pygame_demo/space_invaders.py
@@ -21,6 +21,21 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Ship:
+    COOLDOWN = 30
+
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+
 
 # main game event loop
 def main():
@@ -28,17 +43,21 @@ def main():
     FPS = 60
     level = 1
     lives = 5
-    main_font = pygame.font.SysFont("comicsans", 50)
+    main_font = pygame.font.SysFont("calibri", 50, bold=True)
+    ship = Ship(300, 350)
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
         # draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 0))
-        level_label = main_font.render(f"Level: {level}", 1, (255, 0, 0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (200, 100, 50))
+        level_label = main_font.render(f"Level: {level}", 1, (200, 100, 50))
 
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        ship.draw(WIN)
+
         pygame.display.update()
 
     while run:
