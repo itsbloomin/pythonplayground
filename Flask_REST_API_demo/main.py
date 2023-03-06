65bf6c57082a48a77ea4be13045e21568ebbf1c9
bloomin <steveb1@umbc.edu>
2023-03-05 22:03:07 -0500

    added string parameter to resource

--- Flask_REST_API_demo/main.py
+++ Flask_REST_API_demo/main.py
@@ -6,14 +6,19 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"tim": {"age": 19, "gender": "male"},
+        "bill": {"age": 70, "gender": "male"}}
+
 class HelloWorld(Resource):
-    def get(self):
-        return ({"item" : "Hello World"})
+    def get(self, name):
+        return names[name]
 
     def post(self):
-        return ({"test" : "Posted"})
+        return {"test": "Posted"}
 
-api.add_resource(HelloWorld, "/helloworld")
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+# api.add_resource(HelloWorld, "/helloworld")
 
 if __name__ == "__main__":
     app.run(debug=True)  # turn off in production environment
--- Flask_REST_API_demo/test.py
+++ Flask_REST_API_demo/test.py
@@ -3,6 +3,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "helloworld")
+response = requests.get(BASE + "helloworld")
 print(response.json())
 
