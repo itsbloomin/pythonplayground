67d033b333185a5e898076a9e9a9198b30e6f442
bloomin <steveb1@umbc.edu>
2023-03-06 21:52:13 -0500

    update to space_invaders.py: added losing conditions

--- pygame_demo/space_invaders.py
+++ pygame_demo/space_invaders.py
@@ -80,6 +80,8 @@ def main():
     enemies = []
     wave_length = 5
     enemy_vel = 1
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
@@ -93,10 +95,25 @@ def main():
             enemy.draw(WIN)
 
         player.draw(WIN)
+
+        if lost:
+            lost_label = main_font.render("You Lost!", 1, (200, 100, 50))
+            WIN.blit(lost_label, (WIDTH / 2 - lost_label.get_width() / 2, 350))
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
+        redraw_window()
+
+        if lives <= 0 or player.health <=0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
 
         if len(enemies) == 0:
             level += 1
@@ -107,7 +124,7 @@ def main():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                quit()
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.x - player_vel > 0:
@@ -119,8 +136,11 @@ def main():
         if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
             player.y += player_vel
 
-        for enemy in enemies:
+        for enemy in enemies[:]:     # thats a copy of enemies[]
             enemy.move(enemy_vel)
 
-        redraw_window()
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
 main()
