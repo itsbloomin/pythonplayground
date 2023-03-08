ff5b26be7da07a141e72b4c0557106060ef12e15
bloomin <steveb1@umbc.edu>
2023-03-08 15:27:49 -0500

    i fucking hate oauth

--- Auth/yahoo_oauth_api.py
+++ Auth/yahoo_oauth_api.py
@@ -1,21 +1,18 @@
 # https://pypi.org/project/yahoo-oauth/
 # https://pypi.org/project/yahoo-fantasy-api/
+# https://developer.yahoo.com/fantasysports/guide/
+
 
 from yahoo_oauth import OAuth2
-import yahoo_fantasy_api as yfa
 
 #connect to yahoo api
-sc = OAuth2(None, None, from_file='oauth2.json')
+oauth = OAuth2(None, None, from_file='oauth2.json')
+guid = oauth.guid
 
-#get game object
-gm = yfa.Game(sc, 'nfl')
-leagues = gm.league_ids()
-# print(leagues) # ['348.l.535112', '359.l.676495', '371.l.285372', '380.l.979173', '390.l.789076', '406.l.1123078', '414.l.1014170']
+if not oauth.token_is_valid():
+    oauth.refresh_access_token()
 
-lg = gm.to_league(leagues[0])
-
-print(lg.stat_categories())
-print(lg.team_key())
-
-tm = lg.to_team(lg.team_key())
-print(tm.roster())
+# Example
+# response = oauth.session.get(url, params=payload)
+response = oauth.session.get('https://fantasysports.yahooapis.com/fantasy/v2/game/nfl')
+print(response)
