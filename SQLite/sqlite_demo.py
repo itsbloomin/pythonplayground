83b9efb6be701054a78498297b028f955ed7241b
bloomin <steveb1@umbc.edu>
2023-03-08 18:42:03 -0500

    sqlite_demo.py: testing out SQL. Created first table (customers.db) and those commands to prevent recreation

---
+++ SQLite/sqlite_demo.py
@@ -0,0 +1,28 @@
+# SQLite
+# https://www.sqlite.org/lang.html
+# https://docs.python.org/3/library/sqlite3.html
+# https://www.sqlitetutorial.net/sqlite-python/
+# https://www.tutorialspoint.com/sqlite/sqlite_python.htm
+# YouTube Tutorial: https://www.youtube.com/watch?v=byHcYRpMgI4
+import sqlite3
+
+# connect to database
+conn = sqlite3.connect('customer.db')
+
+# create cursor
+c = conn.cursor()
+
+# create a table (w/DocString)
+# remove / comment out once created -- maybe check if exists?
+# c.execute("""CREATE TABLE customers (
+#                first_name text,
+#                last_name text,
+#                email text
+#         )""")
+
+
+# commit changes
+conn.commit()
+
+# close connection
+conn.close()
