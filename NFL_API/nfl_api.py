c797240a707a2ac3882f6eed9262f5d96a40b777
bloomin <steveb1@umbc.edu>
2023-03-08 12:16:45 -0500

    Refactoring and organizing

--- NFL_API/nfl_api.py
+++
@@ -1,8 +0,0 @@
-# https://developer.yahoo.com/fantasysports/guide/
-# create API Key: https://developer.yahoo.com/apps/create/
-
-# https://developer.yahoo.com/apps/hliwrqgw/
-# App ID: hliwrqgw
-# Client ID (Consumer Key): dj0yJmk9TW1oOFJXVVJLWVo0JmQ9WVdrOWFHeHBkM0p4WjNjbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWJi
-# Client Secret (Consumer Secret): bc76d281b53b6d257bc63fee8324aebbcf39f78f
-# OAuth
