04b36b7f2b4ead34ce9968611371e95af2048152
bloomin <steveb1@umbc.edu>
2023-03-08 20:02:55 -0500

    update.py: update record

---
+++ SQLite/update.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# connect to database
+conn = sqlite3.connect('customer.db')
+
+# create cursor
+c = conn.cursor()
+
+# update records
+c.execute("""UPDATE customers SET first_name = 'John'
+            WHERE rowid = 1
+        """)
+conn.commit()
+
+# query
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
+for item in items:
+    print(item[0], item[1], item[2], item[3])
+
+# commit changes
+conn.commit()
+
+# close connection
+conn.close()
