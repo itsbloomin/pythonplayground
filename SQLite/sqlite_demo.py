4180a6339e6112d7a7df7cf6faaff418fd53af3d
bloomin <steveb1@umbc.edu>
2023-03-08 21:15:45 -0500

    sqlite_demo.py: created functions -show_all() -add_record() -add_many_records() -remove_record() -email_lookup()
    Demo Completed

--- SQLite/sqlite_demo.py
+++ SQLite/sqlite_demo.py
@@ -6,17 +6,61 @@
 # YouTube Tutorial: https://www.youtube.com/watch?v=byHcYRpMgI4
 import sqlite3
 
-# connect to database
-conn = sqlite3.connect('customer.db')
+# Query and Display all
+def show_all():
+    conn = sqlite3.connect('customer.db')  # connect to database
+    c = conn.cursor()  # create cursor
+    c.execute("SELECT rowid, * FROM customers")
+    items = c.fetchall()
+    for item in items:
+        print(item[0], item[1], item[2], item[3])
+    conn.commit()  # commit changes
+    conn.close()  # close connection
 
-# create cursor
-c = conn.cursor()
+# Add many records to table
+def add_record(first, last, email):
+    conn = sqlite3.connect('customer.db')  # connect to database
+    c = conn.cursor()  # create cursor
+    c.execute("INSERT INTO customers VALUES (?, ?, ?)", (first, last, email))
+    conn.commit()  # commit changes
+    conn.close()  # close connection
 
-# query
+# Add new record to table
+def add_many_records(list):
+    conn = sqlite3.connect('customer.db')  # connect to database
+    c = conn.cursor()  # create cursor
+    c.executemany("INSERT INTO customers VALUES (?, ?, ?)", (list))
+    conn.commit()  # commit changes
+    conn.close()  # close connection
 
+# Delete record, use rowid as string
+def remove_record(rowid):
+    conn = sqlite3.connect('customer.db')  # connect to database
+    c = conn.cursor()  # create cursor
+    c.execute("DELETE FROM customers WHERE rowid = (?)", rowid)
+    conn.commit()  # commit changes
+    conn.close()  # close connection
 
-# commit changes
-conn.commit()
+# query with email
+def email_lookup(email):
+    conn = sqlite3.connect('customer.db')  # connect to database
+    c = conn.cursor()  # create cursor
+    c.execute("SELECT * FROM customers WHERE email = (?)", (email,))
+    items = c.fetchall()
+    for item in items:
+        print(item[0], item[1], item[2])
+    conn.commit()  # commit changes
+    conn.close()  # close connection
 
-# close connection
-conn.close()
+# add_record("Jonathan", "Jones", "jonjones@hotmail.com")
+# show_all()
+
+# remove_record('7')
+# show_all()
+
+# customer_list = [("Mike","Smith","mike@smith.com"), ("Megan","Brown","megan@brown.com")]
+# add_many_records(customer_list)
+
+# email_lookup("jonjones@hotmail.com")
+
+show_all()
